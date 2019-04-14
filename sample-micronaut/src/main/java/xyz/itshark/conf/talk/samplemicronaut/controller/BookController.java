d5bc32c1fc353181228b0c6eebff3eee6bb75e11
Vladimir Dejanovic <ed.wong.iv@gmail.com>
2019-04-14 18:09:13 +0200

    security added

--- sample-micronaut/src/main/java/xyz/itshark/conf/talk/samplemicronaut/controller/BookController.java
+++ sample-micronaut/src/main/java/xyz/itshark/conf/talk/samplemicronaut/controller/BookController.java
@@ -3,6 +3,8 @@ package xyz.itshark.conf.talk.samplemicronaut.controller;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import xyz.itshark.conf.talk.samplemicronaut.pojo.Book;
 import xyz.itshark.conf.talk.samplemicronaut.service.BookService;
 
@@ -19,11 +21,13 @@ public class BookController {
 
 
     @Get("/books")
+    @Secured(SecurityRule.IS_ANONYMOUS)
     public List<Book> getAll() {
         return bookService.getAllBooks();
     }
 
     @Get("/admin/book")
+    @Secured(SecurityRule.IS_AUTHENTICATED)
     public Book addBook(@QueryValue("title") String title) {
         return bookService.addBook(title);
     }
---
+++ sample-micronaut/src/main/java/xyz/itshark/conf/talk/samplemicronaut/controller/LoginController.java
@@ -0,0 +1,27 @@
+package xyz.itshark.conf.talk.samplemicronaut.controller;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.views.View;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Secured("isAnonymous()")
+@Controller("/login")
+public class LoginController {
+
+    @Get("/auth")
+    @View("auth")
+    public Map<String, Object> auth() {
+        return new HashMap<>();
+    }
+
+    @Get("/authFailed")
+    @View("auth")
+    public Map<String, Object> authFailed() {
+        return Collections.singletonMap("errors", true);
+    }
+}
